015466563aa2d6c51c803f85cdf85bb972275eba
Maximiliano Alvarenga <maxbispo163@gmail.com>
2020-10-15 21:00:48 -0300

    Day 04
    Day 04 Next Level Week 03

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Lar das meninas",
+    lat: "-27.222633",
+    lng: "-49.6455874",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images:[
+      "https://images.unsplash.com/photo-1531515559935-22d5be677552?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1576025773492-cc2eb828c42a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horário de viditas Das 18h até 8h",
+    open_on_weekends: "1"
+  },
+
+  {
+    id: 2,
+    name: "Lar dos Meninos",
+    lat: "-27.222633",
+    lng: "-49.6555875",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images:[
+      "https://images.unsplash.com/photo-1531515559935-22d5be677552?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1576025773492-cc2eb828c42a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horário de viditas Das 18h até 8h",
+    open_on_weekends: "0"
+
+  }
+]
---
+++ src/server.js
@@ -0,0 +1,24 @@
+// importar dependencias
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+// iniciando o express
+const server = express()
+
+server
+// utilizando os arquivos státicos
+.use(express.static('public'))
+
+// configurar tamplate engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+//ligar servidor
+server.listen(5500)
